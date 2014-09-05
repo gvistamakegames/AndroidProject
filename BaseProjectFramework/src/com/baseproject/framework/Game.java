5ad2f7f717113428cc6107f07b9f5aa65da84655
g_vista <viva.kobe.japan@gmail.com>
2014-09-05 14:49:16 +0900

    Sixth Commit

---
+++ BaseProjectFramework/src/com/baseproject/framework/Game.java
@@ -0,0 +1,20 @@
+package com.baseproject.framework;
+
+// ゲームインターフェース
+public interface Game {
+
+	public Input getInput();
+	
+	public FileIO getFileIO();
+	
+	public Graphics getGraphics();
+	
+	public Audio getAudio();
+	
+	public void setScreen( Screen _screen );
+	
+	public Screen getCurrentScreen();
+	
+	public Screen getStartScreen();
+	
+}
---
+++ BaseProjectFramework/src/com/baseproject/framework/Screen.java
@@ -0,0 +1,23 @@
+package com.baseproject.framework;
+
+public abstract class Screen {
+
+	protected final Game game;
+	
+	public Screen( Game _game ){
+		
+		this.game = _game;
+		
+	}
+	
+	public abstract void update( float _deltaTime );
+	
+	public abstract void present( float _deltaTime );
+	
+	public abstract void pause();
+	
+	public abstract void resume();
+	
+	public abstract void dispose();
+	
+}
